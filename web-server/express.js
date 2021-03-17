b92ddb588a80487a95f64a834b184a154bfc4b88
scottycodehub <fangxia_li@yahoo.com>
2021-03-17 18:30:39 -0400

    weather app version1 done

--- web-server/express.js
+++ web-server/express.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const tools = require('./utils.js')
 const app = express()
 
 //set up hds view engine and views
@@ -11,6 +12,7 @@ hbs.registerPartials(path.join(__dirname,'templates/partials'))
 //set up public static directory
 app.use(express.static(path.join(__dirname,'public')))
 
+const company = "Weather Guru Inc."
 //app default routes
 app.get('', function (req, res) {
   res.render('index', {
@@ -23,25 +25,71 @@ app.get('/help', function (req, res) {
   res.render('help', {
     title:'Help Page',
     content: 'This help me to do nodejs',
-    name:"Scott"
+    name:company
   })
 })
 
 app.get('/about', function (req, res) {
   res.render('about', {
     title:'About Me',
-    name:"Scott"
+    name: company
   })
 })
 
 app.get('/report', function (req, res) {
-  res.send('Weather Report')
+  console.log(req.query)
+  if(req.query.city !== undefined){
+    tools.geocode(req.query.city,(error,{location,latitude,longitude} = {}) => {
+      if(error){
+       return res.send({
+          error
+        })
+      }
+      tools.reportweather(latitude,longitude, (error,response) => {
+          if(error){
+               console.log("Error",error)
+               return res.send(error)
+          }
+          else{
+            res.send({
+              title:'Weather Report',
+              query:req.query.city,
+              name:company,
+              city: location,
+              description: response.current.weather_descriptions[0],
+              temperature: response.current.temperature,
+              feelslike: response.current.feelslike,
+              longitude,
+              latitude
+          })
+            // res.render('weather', {
+            //   title:'Weather Report',
+            //   query:req.query.city,
+            //   name:company,
+            //   city: location,
+            //   description: response.current.weather_descriptions[0],
+            //   temperature: response.current.temperature,
+            //   feelslike: response.current.feelslike,
+            //   longitude,
+            //   latitude
+            // })
+          }
+             
+      })
+  })
+
+  }else{
+    res.send({
+      error: "Not search target provided!"
+    })
+  }
+
 })
 
 app.get('/help/*', function (req, res) {
   res.render('nohelp', {
     title:'About Me',
-    name:"Scott"
+    name:company
   })
 })
 
--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,32 @@
+
 console.log('Client side javascript file is loaded!')
+
+
+const weathersearch = document.querySelector('form')
+const search = document.querySelector("input")
+const msg1 = document.querySelector("#msg1")
+const msg2 = document.querySelector("#msg2")
+
+
+
+weathersearch.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const city = search.value
+    msg1.textContent = 'Loading...'
+    msg2.textContent = ''
+    fetch('/report?city=' + city).then((response) => {
+        response.json().then((data) => {
+            if(data.error){
+                msg1.textContent = data.error
+                msg2.textContent = ''
+            }
+                
+            else{
+                console.log(data)
+                msg1.textContent = 'Report: ' + data.city + ' (Latitude: ' + data.latitude + ' Longitude: ' + data.longitude + ')'
+                msg2.textContent = 'Details: ' + data.description + ' with temperature: ' + data.temperature + ' It feels like: ' + data.feelslike 
+            }
+               
+        })
+    })
+})
